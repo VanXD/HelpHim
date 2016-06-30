0b7977c25499633631f45fddfeb31808809ed2b5
vanxd <forever8187520@vip.qq.com>
2016-06-30 16:14:05 +0800

    事务测试通过

--- admin/src/main/java/com/vanxd/admin/controller/TestController.java
+++ admin/src/main/java/com/vanxd/admin/controller/TestController.java
@@ -1,14 +1,11 @@
 package com.vanxd.admin.controller;
 
+import com.vanxd.admin.service.TestTransactionService;
 import com.vanxd.data.entity.SysRole;
-import com.vanxd.data.entity.SysUser;
-import com.vanxd.data.repository.SysUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.Set;
 
 /**
  * Created by wejoy-a on 2016/6/27.
@@ -16,14 +13,13 @@ import java.util.Set;
 @RestController
 public class TestController {
     @Autowired
-    private SysUserRepository sysUserRepository;
+    private TestTransactionService testTransactionService;
 
     @RequestMapping("/test")
     @ResponseBody
-    public SysUser test() {
-        SysUser one = sysUserRepository.findOne("1");
-        Set<SysRole> sysRoles = one.getSysRoles();
-        return one;
+    public SysRole test() {
+        SysRole save = testTransactionService.save();
+        return save;
     }
 
 
