f6e466a2188d84b036b7461a3d31aec78b6457ea
Van Xiao Dong <373437500@qq.com>
2016-10-12 16:04:32 +0800

    数据库结构同步

--- data/src/main/resources/TableStructure.sql
+++ data/src/main/resources/TableStructure.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50616
 File Encoding         : 65001
 
-Date: 2016-09-19 16:30:35
+Date: 2016-10-12 16:04:09
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -33,7 +33,8 @@ CREATE TABLE `sys_permission` (
   `icon` varchar(20) DEFAULT NULL,
   `url` varchar(255) DEFAULT NULL,
   `parent_id` varchar(32) DEFAULT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  KEY `index_parent_id` (`parent_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -45,9 +46,9 @@ CREATE TABLE `sys_role` (
   `create_time` datetime NOT NULL,
   `status` int(11) NOT NULL,
   `description` varchar(300) NOT NULL,
-  `isshow` bit(1) NOT NULL,
   `name` varchar(100) NOT NULL,
   `role` varchar(100) NOT NULL,
+  `creator_user_id` varchar(32) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -58,7 +59,8 @@ DROP TABLE IF EXISTS `sys_role_permission`;
 CREATE TABLE `sys_role_permission` (
   `role_id` varchar(32) NOT NULL,
   `permission_id` varchar(32) NOT NULL,
-  PRIMARY KEY (`role_id`)
+  PRIMARY KEY (`role_id`),
+  KEY `index_role_permission` (`role_id`,`permission_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -69,14 +71,14 @@ CREATE TABLE `sys_user` (
   `id` varchar(32) NOT NULL,
   `create_time` datetime NOT NULL,
   `status` int(11) NOT NULL,
-  `admin` bit(1) NOT NULL,
   `email` varchar(100) NOT NULL,
   `nickname` varchar(100) NOT NULL,
   `mobile_phone` varchar(11) NOT NULL,
   `password` varchar(100) NOT NULL,
   `salt` varchar(5) NOT NULL,
   `username` varchar(100) NOT NULL,
-  PRIMARY KEY (`id`)
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_username` (`username`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- ----------------------------
@@ -86,6 +88,7 @@ DROP TABLE IF EXISTS `sys_user_role`;
 CREATE TABLE `sys_user_role` (
   `user_id` varchar(32) NOT NULL,
   `role_id` varchar(32) NOT NULL,
-  PRIMARY KEY (`user_id`)
+  PRIMARY KEY (`user_id`),
+  KEY `index_user_role` (`user_id`,`role_id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 SET FOREIGN_KEY_CHECKS=1;
