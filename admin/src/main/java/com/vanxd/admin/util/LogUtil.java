2eaecc0cb3a683e51c6fba2f9e7896111919c690
Van Xiao Dong <373437500@qq.com>
2017-02-21 10:01:06 +0800

    日志工具类

---
+++ admin/src/main/java/com/vanxd/admin/util/LogUtil.java
@@ -0,0 +1,12 @@
+package com.vanxd.admin.util;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author wyd on 2017/2/21.
+ */
+public class LogUtil {
+    public static void errorLog(Class clazz, String msg) {
+        Logger.getLogger(clazz).error(msg);
+    }
+}
