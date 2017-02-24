4005b85e66345de55c28898897721a733dc07f15
Van Xiao Dong <373437500@qq.com>
2017-02-24 11:36:04 +0800

    修改了数据，就清除缓存

--- admin/src/main/java/com/vanxd/admin/service/article/ArticleService.java
+++ admin/src/main/java/com/vanxd/admin/service/article/ArticleService.java
@@ -5,10 +5,14 @@ import com.vanxd.data.dict.redis.key.RedisKeyConstant;
 import com.vanxd.data.entity.article.Article;
 import com.vanxd.data.mapper.article.ArticleMapper;
 import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
 import org.springframework.cache.annotation.Cacheable;
 
 public interface ArticleService extends BaseService<Article, ArticleMapper> {
+
+    @CacheEvict(value = RedisKeyConstant.ARTICLE_DETAIL, key = "#entity.id")
+    @Override
+    boolean edit(Article entity);
+
     @Cacheable(value = RedisKeyConstant.ARTICLE_DETAIL, key = "#id")
     @Override
     Article findByPrimaryKey(String id);
