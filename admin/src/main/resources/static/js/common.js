e402740fe06d39654d762ffb764c4cc8676a9151
vanxd <373437500@qq.com>
2017-05-13 11:09:43 +0800

    小优化

--- admin/src/main/resources/static/js/common.js
+++ admin/src/main/resources/static/js/common.js
@@ -59,30 +59,6 @@ function bindIChecks(selector) {
     }
 }
 
-function initDateFormatter() {
-    // 对Date的扩展，将 Date 转化为指定格式的String
-    // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
-    // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
-    // 例子：
-    // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
-    // (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
-    Date.prototype.format = function (fmt) { //author: meizz
-        var o = {
-            "M+": this.getMonth() + 1, //月份
-            "d+": this.getDate(), //日
-            "h+": this.getHours(), //小时
-            "m+": this.getMinutes(), //分
-            "s+": this.getSeconds(), //秒
-            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
-            "S": this.getMilliseconds() //毫秒
-        };
-        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-        for (var k in o)
-            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
-        return fmt;
-    }
-}
-
 /**
  * 浏览器尺寸改变时，改变jqGrid的宽度
  */
@@ -187,55 +163,53 @@ var jqGridFactory = {
             colModel : data.colModel
         });
         // Setup buttons
-        $(data.tableSelector).jqGrid('navGrid', '#' + data.pager,
-            {
-                addfunc : id => {
-                    var editModalForm = $("#edit-modal-form");
-                    editModalForm.find("input:not([type=checkbox]), input:not([type=radio])").val("");
-                    editModalForm.find("input:checked").iCheck("uncheck");
-                    addFuncDiaglog(id);
-                    editModalForm.modal();
-                },
-                editfunc  : id => {
-                    $.ajax({
-                        type : "GET",
-                        url  : "getById.json",
-                        data : {
-                            id : id
-                        },
-                        success : result => {
-                            if(isRequestSuccess(result)) {
-                                addPrimaryKeyInput(id);
-                                renderData(result.result);
-                                editFuncDiaglog(result.result);
-                                $("#edit-modal-form").modal();
-                            } else {
-                                handleRequestFail(result);
-                            }
+        $(data.tableSelector).jqGrid('navGrid', '#' + data.pager, {
+            addfunc : id => {
+                var editModalForm = $("#edit-modal-form");
+                editModalForm.find("input:not([type=checkbox]), input:not([type=radio])").val("");
+                editModalForm.find("input:checked").iCheck("uncheck");
+                addFuncDiaglog(id);
+                editModalForm.modal();
+            },
+            editfunc  : id => {
+                $.ajax({
+                    type : "GET",
+                    url  : "getById.json",
+                    data : {
+                        id : id
+                    },
+                    success : result => {
+                        if(isRequestSuccess(result)) {
+                            addPrimaryKeyInput(id);
+                            renderData(result.result);
+                            editFuncDiaglog(result.result);
+                            $("#edit-modal-form").modal();
+                        } else {
+                            handleRequestFail(result);
                         }
-                    });
-                },
-                delfunc : id => {
-                    $.ajax({
-                        type : "POST",
-                        url  : "delete.json",
-                        data : {
-                            id : id
-                        },
-                        success : result => {
-                            if(isRequestSuccess(result)) {
-                                $(DEFAULT_JQ_GRID).trigger("reloadGrid");
-                            } else {
-                                handleRequestFail(result);
-                            }
+                    }
+                });
+            },
+            delfunc : id => {
+                $.ajax({
+                    type : "POST",
+                    url  : "delete.json",
+                    data : {
+                        id : id
+                    },
+                    success : result => {
+                        if(isRequestSuccess(result)) {
+                            $(DEFAULT_JQ_GRID).trigger("reloadGrid");
+                        } else {
+                            handleRequestFail(result);
                         }
-                    });
-                },
-                alerttext  : "请选中需要操作的数据行！"
-            },{},{},{},{
-                multipleSearch : true
-            }
-        );
+                    }
+                });
+            },
+            alerttext  : "请选中需要操作的数据行！"
+        },{},{},{},{
+            multipleSearch : true
+        });
         return DEFAULT_JQ_GRID;
     }
 };
@@ -380,3 +354,27 @@ var UEditorFactory = {
         return UE.getEditor(_domId);
     }
 };
+
+function initDateFormatter() {
+    // 对Date的扩展，将 Date 转化为指定格式的String
+    // 月(M)、日(d)、小时(h)、分(m)、秒(s)、季度(q) 可以用 1-2 个占位符，
+    // 年(y)可以用 1-4 个占位符，毫秒(S)只能用 1 个占位符(是 1-3 位的数字)
+    // 例子：
+    // (new Date()).Format("yyyy-MM-dd hh:mm:ss.S") ==> 2006-07-02 08:09:04.423
+    // (new Date()).Format("yyyy-M-d h:m:s.S")      ==> 2006-7-2 8:9:4.18
+    Date.prototype.format = function (fmt) { //author: meizz
+        var o = {
+            "M+": this.getMonth() + 1, //月份
+            "d+": this.getDate(), //日
+            "h+": this.getHours(), //小时
+            "m+": this.getMinutes(), //分
+            "s+": this.getSeconds(), //秒
+            "q+": Math.floor((this.getMonth() + 3) / 3), //季度
+            "S": this.getMilliseconds() //毫秒
+        };
+        if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
+        for (var k in o)
+            if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, (RegExp.$1.length == 1) ? (o[k]) : (("00" + o[k]).substr(("" + o[k]).length)));
+        return fmt;
+    }
+}
